195f0c5ca458e7b749f4fd050e5dbe3a1af5b4cb
JeongSubi <jsb97613@naver.com>
2025-04-22 18:19:03 +0900

    chore: initialize project structure with NestJS and base directories

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { startApplication } from '@src/app';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+startApplication()
+  .then((): void => {
+    console.log(`Process Start!!!`);
+  })
+  .catch((error: unknown): void => {
+    console.log(`Process Start Error!!! ${error}`);
+  });
